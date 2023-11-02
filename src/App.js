e08245828f30bbc8bbbd53d59bf0ab566cf6ae20
ski107 <148421960+ski107@users.noreply.github.com>
2023-11-01 21:20:57 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from 'react';
 
-function sendVote() {
-  fetch('/api/vote')
-    .then(response => response.json())
-    .then(json => alert(json.text));
-}
-
 function App() {
-  return (
-    <div className="App">
-      <button onClick={sendRequest}>Vote</button>
-    </div>
-  );
+  const value = 'World';
+  return <div>Hello {value}</div>;
 }
 
 export default App;
